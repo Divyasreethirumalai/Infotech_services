c744e8822d2a5f0be6755ab1b05c7a5113c0d0f2
Sree <sreeiit123@gmail.com>
2026-05-28 00:12:45 +0530

    updted hover effects and some ui

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.getElementById("navLinks");
+const mobileMenu = document.getElementById("mobileMenu");
+const closeMenu = document.getElementById("closeMenu");
 
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+});
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+});
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
 });
